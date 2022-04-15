35949bbb86f1aac5056a319f543859785290a3a5
ChristianGhosn <95905516+ChristianGhosn@users.noreply.github.com>
2022-04-15 22:12:41 +1000

    Initial Commit

---
+++ LearningWithGitHub/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  LearningWithGitHub
+//
+//  Created by Christian Ghosn on 15/4/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ LearningWithGitHub/LearningWithGitHubApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningWithGitHubApp.swift
+//  LearningWithGitHub
+//
+//  Created by Christian Ghosn on 15/4/2022.
+//
+
+import SwiftUI
+
+@main
+struct LearningWithGitHubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
